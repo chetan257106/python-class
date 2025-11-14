62be548c4e6ce089527a50ec9b53b96a442806b8
chetan chauhan <chetan_257106@saitm.ac.in>
2025-11-14 12:39:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,425 @@
+print("hello chetan chauhan")
+
+# a = int(input("enter your value"))
+# b = int(input("enter your value"))
+# c = a+b
+# print(c)
+
+
+
+# #####################LIST CONCECP
+
+# logic = ["apple","banana","gauva","orange"]
+# print(logic[1])
+
+# logic.append("patato")
+
+# logic.append("lofgd")
+# print(logic)
+
+# logic.insert(2,"amrud")
+# print(logic)
+
+
+# logic=["apple","banana","3","4","qewrtyuhj"]
+
+# logic[2]="abs"
+# print(logic)
+# print("lkjhgfcvbnm")
+
+# logic[1:3]=["rajputy"]
+# print(logic)
+
+# logic=[1,2,4,5,6,]
+
+# logic.append(3)
+
+
+# logic.insert(2,3)
+# print(logic)
+
+# logic.append(3)
+# print(logic)
+
+# ###extend mean adding two lists
+
+# list1=[1,2,3,4,5,6]
+
+# list2=[7,8,9]
+
+
+# list1.extend(list2)
+# print(list1)
+
+
+# list3=[1,2,3,"wert","vbgesxcvgfresx"]
+
+# list3.remove("wert")
+# print(list3)
+
+# list3.pop(3)
+# print(list3)
+
+
+
+# logics=[1,2,3,4,5]
+
+# logics.append("apple")
+# print(logics)
+
+# logics.insert(5,"mango")
+# print(logics)
+
+# logics.remove("apple")
+# print(logics)
+
+# logics.pop(5)
+# print(logics)
+
+# logics.clear()
+# print(logics)
+# list=[]
+
+# list.append("apple")
+
+# list.append(235678)
+
+# list.append(23)
+
+# list.append("wer")
+
+# list.append("amrud")
+
+# list.append("mangoes")
+
+# print(list)
+
+# list.insert(1,45)
+# print(list)
+
+# list[0]="hello america "
+# print(list)
+
+# ###############LOOPS STICKS
+
+# list1=[5,4,3,2,1,6]
+
+# for x in list1:
+#     print(x)
+
+# list1.sort()
+# print(list1)
+
+
+# list1.sort(reverse=True)
+# print(list1)
+# list1.sort(reverse=False)
+# print(list1)
+
+# python = "the est lanGUage ever made"
+# c = "it is tOO complecated LANGuage"
+
+# print(python.upper())
+# print(python.lower())
+# print(python.strip())
+# print(python.replace("est","best"))
+# print(python.split())
+
+
+
+
+# ##format string
+
+
+
+# name = "chetan"
+# age = 345
+# print(f"my name is {name} and i am {age} years old")
+# print("my name is {} i am {} i am years old")
+    
+# x = 5
+# x+=4
+# print(x)
+  
+
+# y = 34
+# y*=23
+# print(y)
+
+
+# z = 34
+# z/=3
+# print(z)
+# a = 123
+# a-=12
+# print(a)
+
+
+# b = 23
+# b%=23
+# print(b)
+
+
+
+
+# a = 23
+# b = 234
+
+# print(a==b)#false
+# print(a!=b)#true
+# print(a>b)#false
+# print(a<b)#true
+# print(a>=b)#false
+# print(a<=b)#true
+
+
+
+# x = 234
+# y = 234
+# z = 12
+# a  = 23
+
+# print(x>10 and y>10)
+# print(x<10 and y>10)
+
+
+
+
+# x = [1,2,3]
+# y = x
+# z = [1,2,3]
+
+# print(x is y)
+# print(x is not y)
+# print(x is z)
+# print(x is not z)
+# print(y is z)
+# print(y is not z)
+
+
+
+# a = 5
+# b = 240
+# print(a*b)
+
+
+
+# a = {"a","a","b","c"}
+# print(a)
+# for x in a:
+#     print(x)
+# #print(a)
+# a = ("a","a","b","c")
+# for x in a:
+#     print(a)
+
+# dict = {
+#     "student":"chetan",
+#     "classs":12,
+#      "subject":"math"
+# }
+# print(dict)
+# dict["age"]=23,
+# print(dict)
+# for key,value in dict.items():
+#     print(key,":",value)
+
+
+
+# #concept of if and else
+
+# a =int(input("enter your value"))
+# if a>=18:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# #usses of loops in if and else condition
+
+# for i in range(80):
+#     a = int(input("enter your age:"))
+#     if 100>=a>=18:
+#         print("yes you can vote ")
+#     elif a>100:
+#         print("you have entered incorrect age ".upper())
+#     else:
+#         print("no you are a baby right now ")
+ 
+ 
+# for i in range(12):
+#     light =str(input("input your colour"))
+#     if light == "green":
+#         print("yes you can go")
+#     else:
+#         print("invalid value")
+
+
+
+
+# x = 15
+# if x>10:
+#     print("true")
+#     if x>20:
+#         print("false")
+#     else:
+#         print("yes")
+
+
+
+# for i in range(0):
+#     y = int(input("enter your value"))
+# if y%2 ==0:
+#     print("value is even")
+# else:
+#     print("odd")
+
+
+
+# #write a program to find largest three numn using if ilif else
+
+# #llllooooppppssss
+
+# fruits = ["mango","gauva","apple"]
+
+# for x in fruits:
+#     print(x)
+
+# word = "python"
+# for x in word:
+#     print(x)
+# for i in range(1,10,2):
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+
+# for i in range(1,35,2):
+#     print(i)
+
+# for i in range(5,12):
+#     print(i)
+
+
+# for i in range(0,3):
+#  for j in range(1,2):
+#   for k in range(1,2):
+#    print(f"i={i},j={j},k={k}")
+
+
+# for i in range(1,6):
+#    if i==6:
+#       break
+#    print(i)
+
+# for i in range(1,6):
+#     if i ==3:
+#         continue
+#     print(i)
+
+
+# for i in range(1,21):
+#     print(i)
+
+
+# for i in range(2,29,2):
+#     print(i)
+
+
+# list =["yellow","blue","red","black"]
+# for x in  list:
+#   print(x)
+
+
+#ffuunnccttiioonn
+
+
+# def greet():
+#     print("hello,python!")
+
+# greet()
+
+# def greet(name):
+#     print(f"hello,{name}!")
+# greet("alice")
+
+# greet("hiiiii")
+
+# def add(a,b):
+#  #   return a+b
+#     return a*b
+#     return a/b
+#     return a-b
+#     return a%b
+# #result1=add(12,7)
+# result2=multiply(12,4)
+# result3=divide(12,7)
+# result4=substract(12,7)
+# result5=remainder(12,7)
+## print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
+# print(result5)
+
+
+
+# def greet(name="student"):
+#     print(f"let's play,{name}!")
+# greet("BGMI")
+
+
+# x = 10 #global
+# def fun():
+#     y = 5 #local
+#     print(x,y)
+# fun()
+# print(x)
+# print(y)#############################3when yellow line is coming then warning start
+
+
+# def greet():
+#     print("hello!")
+# greet()
+
+# def add(a,b):
+#     return a+b
+# result1=add(1234,123)
+# print(result1)
+
+
+# x= 233
+# def fun():
+#     y = 123
+#     print(x,y)
+# fun()
+# print(x)
+# print(y)
+
+
+
+
+
+
+###########loops in python
+
+#class
+# class Car:
+#     def __init__(self,brand,color):
+        
+#      self.brand=brand #attribute
+#      self.color=color#attribute
+
+#     def drive(self):#method
+#      print(f'{self.color}{self.brand}is driving 🚗💕💕')
+
+# #objects
+# car1=Car("BMW","black")
+
+
+# car2=Car("Tesla","White")
+
+# car1.drive()
+# car2.drive()
+
